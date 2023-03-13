891362fdd88f8522cbb4e2f5bfbea85d8553ccb3
Ivan Petrov <ivan.petrov@gmail.com>
2023-03-13 19:55:37 +0200

    Init

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title>PHP base</title>
+</head>
+<body>
+    <div class="main">
+        <h1>
+            <?php
+                // Выводим на экран строчку
+                # Другой вариант комментария
+
+                /*
+                    Многострочные
+                    комментарии
+                 */
+                echo 'Hello world!';
+            ?>
+            <?= 'Hello world!'; ?>
+        </h1>
+    </div>
+</body>
+</html>
